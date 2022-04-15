ffe8f5919cacf845c65630e249ac1b5ebadb5971
lodashreload <icarusjapan@gmail.com>
2022-04-15 16:56:00 -0400

    Any change to count causes out functions to be called

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export function App() {
-  return <div>Hello, World </div>
+  const [count, setCount] = useState(0)
+  // const listOfDataToWatchForChanges = [count] ..Don't need if called in line below
+  // useEffect takes two arguments a function AND an array of values to watch for changes
+  useEffect(theCountChanged, [count])
+
+  function handleClickButton() {
+    setCount(count + 1)
+  }
+  function theCountChanged() {
+    console.log(`Wow, the count changed and is now ${count}`)
+  }
+  return (
+    <div>
+      <p>
+        Count: {count} <button onClick={handleClickButton}>Click Me</button>
+      </p>
+    </div>
+  )
 }
